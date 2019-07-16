22e3e51cb2fedef4e8eb571680dc0c75c65c4582
Natasha Waller <natashawaller@Natashas-MacBook-Pro.local>
2019-07-16 11:00:31 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,14 @@ class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
   def index
+    #This code is asking the Student model in models/student.rb to retrieve all of
+    #the students from the database and then save them in an instance variable 
+    #called @students.
     @students = Student.all
+    
+    #Once @students is defined, the controller calls the app/views/students/index.html.erb
+    #file and is able to iterate through @students and render them in HTML
+    #according to how it is structured. 
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,9 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
 end
+
+#The code above is essentially saying: Hey Rails! Connect the incoming request
+#for my students resources only to the index, new, and create actions listed in my
+#students_controller file. Then it will connect the request for students with a particular
+#id number to the show action in the students_controller file and refer to this 
+#path as simply /student instead of students/:id 
